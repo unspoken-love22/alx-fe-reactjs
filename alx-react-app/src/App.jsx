6549806efd6a8d355a94ce9fd88b07b71f327e6c
IBYIMANISHAKA Danny <222006709danny@gmail.com>
2025-02-20 08:17:23 +0200

    Create UserProfile component and use it in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,6 +7,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 
 
@@ -42,6 +44,9 @@ function App() {
             <Header />
             <MainContent />
             <Footer />
+        </div>
+        <div>
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         </div>
 
     </>
