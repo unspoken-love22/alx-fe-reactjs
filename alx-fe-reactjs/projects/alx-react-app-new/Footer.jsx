e5f7619ba34bf7f950cd73fb0c26a61ff191e2e4
IBYIMANISHAKA Danny <222006709danny@gmail.com>
2025-03-01 11:11:11 +0200

    Reorganize project structure

---
+++ alx-fe-reactjs/projects/alx-react-app-new/Footer.jsx
@@ -0,0 +1,12 @@
+// Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2025 My Favorite Cities</p>
+    </footer>
+  );
+};
+
+export default Footer;
